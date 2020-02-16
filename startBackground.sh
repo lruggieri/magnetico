c78d5fd2a22fb98649c8d0190ad17e32835ade7a
Luca Ruggieri <luc4.ruggieri@gmail.com>
2020-02-16 19:47:49 +0100

    old script removed

--- startBackground.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-script_path=$(dirname "$0")
-cd $script_path
-go build --tags "fts5" "-ldflags=-s" -o $script_path/magneticod $script_path/cmd/magneticod/main.go;
-
-$script_path/magneticod -v --database=beanstalk://164.68.122.92:11300/magneticod_tube --indexer-max-neighbors=50000 --indexer-interval=2
