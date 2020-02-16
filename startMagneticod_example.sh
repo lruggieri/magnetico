1a59da48a65cc024ec4de0d9aebc9df74eab807b
Luca Ruggieri <luc4.ruggieri@gmail.com>
2020-02-16 20:00:57 +0100

    README updated to start magneticod as a service

--- startMagneticod_example.sh
+++ startMagneticod_example.sh
@@ -3,4 +3,4 @@ script_path=$(dirname "$0")
 cd $script_path
 go build --tags "fts5" "-ldflags=-s" -o $script_path/magneticod $script_path/cmd/magneticod/main.go;
 
-$script_path/magneticod -v --database=beanstalk://164.68.122.92:11300/magneticod_tube --indexer-max-neighbors=50000 --indexer-interval=2
+$script_path/magneticod -v --database=beanstalk://yourBeansTalkIP:yourBeansTalkPort/magneticod_tube --indexer-max-neighbors=50000 --indexer-interval=2
